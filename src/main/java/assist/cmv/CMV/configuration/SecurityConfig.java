c701dc7e5fc836b8800a935037db3566911889f7
Maruneac Cosmin <cosmin.maruneac@raisissoftware.ro>
2021-04-05 23:36:41 +0300

    Update: LOGIN

--- src/main/java/assist/cmv/CMV/configuration/SecurityConfig.java
+++ src/main/java/assist/cmv/CMV/configuration/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -45,18 +44,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                //.antMatchers("/authenticate")
-                .antMatchers()
+                .antMatchers("/authenticate")
+                //.antMatchers()
                 .permitAll()
                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .exceptionHandling()
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                .permitAll();
-       //http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
-        http.cors().disable();
+                .authenticated()
+               .and()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //.permitAll();
+       http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+       // http.cors().disable();
     }
 }
--- src/main/java/assist/cmv/CMV/controller/WebController.java
+++ src/main/java/assist/cmv/CMV/controller/WebController.java
@@ -1,6 +1,7 @@
 package assist.cmv.CMV.controller;
 
 import assist.cmv.CMV.model.AuthRequest;
+import assist.cmv.CMV.repository.UserRepository;
 import assist.cmv.CMV.service.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -23,6 +26,8 @@ public class WebController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/authenticate")
     public ResponseEntity generateToken(@RequestBody AuthRequest authRequest) throws Exception{
@@ -33,6 +38,13 @@ public class WebController {
             throw new Exception("Invalid username/password");
         }
 
-        return new ResponseEntity(jwtUtil.generateToken(authRequest.getEmail()), HttpStatus.OK);
+
+        HashMap<String, Object> hmap = new HashMap<String, Object>();
+        hmap.put("token", jwtUtil.generateToken(authRequest.getEmail()));
+        hmap.put("id",userRepository.findByEmail(authRequest.getEmail()));
+
+        return new ResponseEntity<HashMap<String, Object>>(hmap, HttpStatus.OK);
+
+       // return new ResponseEntity(jwtUtil.generateToken(authRequest.getEmail()), HttpStatus.OK);
     }
 }
